c7056ac2debdbee066fd00501c17bc252af6dd16
agiledev0115 <alexanderagiledev@gmail.com>
2023-02-15 23:06:17 +0800

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -29,34 +29,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late VideoPlayerController _controller;
+  VideoPlayerController? _controller;
+  late Future<void> _initializeVideoPlayerFuture;
   // late VideoPlayerController _controller1;
   String url = '';
   String url1 = '';
   @override
   void initState() {
+    initPlayer();
     super.initState();
-    getInstaUrl();
   }
 
-  void getInstaUrl() async {
+  Future initPlayer() async {
     final List<InstaPost> post = await FlutterInsta().getPostData(
         "https://www.instagram.com/reel/CohqWpyDNSm/?utm_source=ig_web_copy_link");
     // final List<InstaPost> post1 = await FlutterInsta().getPostData(
     //     "https://www.instagram.com/tv/CTfdcAWB1AM/?utm_source=ig_web_copy_link");
     url = post[0].displayURL.toString();
     // url1 = post1[0].displayURL.toString();
-    _controller = VideoPlayerController.network(url)
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
+    _controller = VideoPlayerController.network(url);
+    _initializeVideoPlayerFuture = _controller!.initialize()
+      ..then((_) {
+        setState(() {
+          _controller!.play();
+        });
       });
+    return _initializeVideoPlayerFuture;
     // _controller1 = VideoPlayerController.network(
     //     'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
     //   ..initialize().then((_) {
     //     // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
     //     setState(() {});
     //   });
+  }
+
+  bool get isVideoPlaying {
+    return _controller?.value?.isPlaying != null &&
+        _controller!.value.isPlaying;
   }
 
   @override
@@ -75,13 +84,15 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
+            if (isVideoPlaying) {
+              _controller?.pause();
+            } else {
+              _controller?.play();
+            }
           });
         },
         child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          isVideoPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
@@ -89,17 +100,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _dialogBuilder(
       BuildContext context,
-      VideoPlayerController
+      VideoPlayerController?
           _controller /*, VideoPlayerController _controller1*/) {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           insetPadding: EdgeInsets.zero,
-          content: _controller.value.isInitialized
+          content: _controller != null && _controller!.value!.isInitialized
               ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
+                  aspectRatio: _controller!.value!.aspectRatio,
+                  child: VideoPlayer(_controller!),
                 )
               : Container(),
           actions: <Widget>[
